a525beea1e570dc62e79a85d85e45d051df5a948
tianci.ltc <tianci.ltc@alibaba-inc.com>
2018-11-04 19:19:22 +0800

    finish generateHash

--- stream/concatFile.js
+++
@@ -1,8 +0,0 @@
-const fromArray = require('from2-array');
-const through = require('through2');
-const fs = require('fs');
-
-function concatFiles(destination, files, callback){
-  const destStream = fs.createWriteStream(destination);
-  fromArray.obj()
-}
