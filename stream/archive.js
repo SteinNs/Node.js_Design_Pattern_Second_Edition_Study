4abf760a7b774920fbd0f4f52c80cf1edfd0afb3
tianci.ltc <tianci.ltc@alibaba-inc.com>
2018-11-04 18:16:20 +0800

    finish archive with combine

--- stream/archive.js
+++ stream/archive.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const combine = require('combine');
 const compressAndEncryptStream = require('./combinedStreams').compressAndEncrypt;
-fs.createReadStream(process.argv[3])
+combine(
+  fs.createReadStream(process.argv[3])
   .pipe(compressAndEncryptStream(process.argv[2]))
   .pipe(fs.createWriteStream(process.argv[3]+ '.gz.enc'))
-  .on('error', err => {
+).on('error', err => {
     console.log(err);
   })
 
-  
